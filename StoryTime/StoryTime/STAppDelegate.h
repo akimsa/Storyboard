cda74eb2d585fef23accc687ccdd8e0068e3fe5d
*.* <x@x.com>
2012-08-29 22:46:02 -0500

    initial push

---
+++ StoryTime/StoryTime/STAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  STAppDelegate.h
+//  StoryTime
+//
+//  Created by Akim Salem-Atia on 8/29/12.
+//  Copyright (c) 2012 Akim Salem-Atia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ StoryTime/StoryTime/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StoryTime
+//
+//  Created by Akim Salem-Atia on 8/29/12.
+//  Copyright (c) 2012 Akim Salem-Atia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "STAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([STAppDelegate class]));
+    }
+}
